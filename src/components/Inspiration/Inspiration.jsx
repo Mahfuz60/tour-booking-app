45228526b1195eec6124cbff94191649e9231fba
mahfuz60 <mahfuzuralam1996@gmail.com>
2022-06-24 18:44:04 +0600

    home properties

--- src/components/Inspiration/Inspiration.jsx
+++ src/components/Inspiration/Inspiration.jsx
@@ -3,35 +3,26 @@ import './Inspiration.css';
 
 const Inspiration = () => {
   return (
-    <div className='inspiration'>
-      <div className='insirationItem'>
-        <img
-          className='inspirationImg'
-          src='https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o='
-          alt=''
-        />
-        <div className='inspirationTitle'>
-          <h3>Dublin</h3>
+    <div className="container">
+      <h3 className='inspirationHeader py-3'>Get inspiration for your next trip</h3>
+      <div className='inspiration'>
+        <div className='insirationItem'>
+          <img className='inspirationImg' src='https://i.hurimg.com/i/hdn/75/0x0/5ab0ff9ad3806d12540aada2.jpg' alt='' />
+          <div className='inspirationTitle'>
+            <h3>Istanbul, Turkey</h3>
+          </div>
         </div>
-      </div>
-      <div className='insirationItem'>
-        <img
-          className='inspirationImg'
-          src='https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o='
-          alt=''
-        />
-        <div className='inspirationTitle'>
-          <h3>Reno</h3>
+        <div className='insirationItem'>
+          <img className='inspirationImg' src='https://www.rishirajholidays.com/wp-content/uploads/kasmir.jpg' alt='' />
+          <div className='inspirationTitle'>
+            <h3>Kasmir,India</h3>
+          </div>
         </div>
-      </div>
-      <div className='insirationItem'>
-        <img
-          className='inspirationImg'
-          src='https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o='
-          alt=''
-        />
-        <div className='inspirationTitle'>
-          <h3>Austin</h3>
+        <div className='insirationItem'>
+          <img className='inspirationImg' src='https://pbs.twimg.com/media/EihEAn4XgAADVQk.jpg' alt='' />
+          <div className='inspirationTitle'>
+            <h3>Ladak,India</h3>
+          </div>
         </div>
       </div>
     </div>
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Features from '../../Features/Features';
+import FeaturesProperties from '../../FeaturesProperties/FeaturesProperties';
 import Header from '../../Header/Header';
 import Inspiration from '../../Inspiration/Inspiration';
+import MailList from '../../MailList/MailList';
 import Property from '../../Property/Property';
 import './Home.css';
 
@@ -11,9 +13,10 @@ const Home = () => {
       <Header />
       <div className='homeContainer'>
         <Features />
-        <Property />
-        <h3 className='inspirationHeader'>Get inspiration for your next trip</h3>
         <Inspiration />
+        <Property />
+        <FeaturesProperties />
+        <MailList/>
       </div>
     </div>
   );
